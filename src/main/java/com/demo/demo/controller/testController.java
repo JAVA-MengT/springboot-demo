c30de876194eb52ce1ba01d8fd1c2aa3143a51d5
zengjinlin <zeng.jl@neusoft.com>
2018-11-03 22:35:25 +0800

    master

--- src/main/java/com/demo/demo/controller/testController.java
+++ src/main/java/com/demo/demo/controller/testController.java
@@ -10,8 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class testController {
     @RequestMapping("/")
-    public String test1() {
-        System.out.println("master1");
-        return "master1";
+    public String test2() {
+        System.out.println("master2");
+        return "master2";
     }
 }
