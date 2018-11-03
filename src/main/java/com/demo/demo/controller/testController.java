b59944c8cebb968932664a6f2c0222d41df41228
zengjinlin <zeng.jl@neusoft.com>
2018-11-03 21:36:16 +0800

    master

--- src/main/java/com/demo/demo/controller/testController.java
+++ src/main/java/com/demo/demo/controller/testController.java
@@ -11,6 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class testController {
     @RequestMapping("/")
     public String test() {
-        return "branch2.1";
+        return "master";
     }
 }
