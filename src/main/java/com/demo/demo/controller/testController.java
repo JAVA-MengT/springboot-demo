c7094796abf3a4471e39bb35cb689701dbf6a37c
zengjinlin <zeng.jl@neusoft.com>
2018-11-03 20:49:46 +0800

    初始化

---
+++ src/main/java/com/demo/demo/controller/testController.java
@@ -0,0 +1,16 @@
+package com.demo.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zengjinlin
+ * @create 2018-11-03 20:46
+ */
+@RestController
+public class testController {
+    @RequestMapping("/")
+    public String test() {
+        return "master";
+    }
+}
