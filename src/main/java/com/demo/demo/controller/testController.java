dacfe763800d608a767ec89292235982593a6e8c
zengjinlin <zeng.jl@neusoft.com>
2018-11-03 21:22:56 +0800

    branch1.1.2

--- src/main/java/com/demo/demo/controller/testController.java
+++ src/main/java/com/demo/demo/controller/testController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class testController {
     @RequestMapping("/")
     public String test() {
-        System.out.println("21:04");
+        System.out.println("branch1.1.2");
         return "branch1.1.2";
     }
 }
