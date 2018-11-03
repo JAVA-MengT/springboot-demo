02c1f9ff4eebeacb7a035912d92bfda5b1acedb0
zengjinlin <zeng.jl@neusoft.com>
2018-11-03 23:05:14 +0800

    branch2.1

--- src/main/java/com/demo/demo/controller/testController.java
+++ src/main/java/com/demo/demo/controller/testController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class testController {
     @RequestMapping("/")
     public String test() {
-        System.out.println("branch1.1.1_1");
-        return "branch1.1.1_1";
+        System.out.println("branch1.1.1_2");
+        return "branch1.1.1_2";
     }
 }
