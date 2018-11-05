bdef83bbbf3ca96f2114b0deda3e132ef7d7388f
zengjinlin <zeng.jl@neusoft.com>
2018-11-05 15:58:46 +0800

    master

---
+++ src/main/java/com/demo/demo/controller/test1Controller.java
@@ -0,0 +1,17 @@
+package com.demo.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zengjinlin
+ * @create 2018-11-03 20:46
+ */
+@RestController
+public class test1Controller {
+    @RequestMapping("/")
+    public String test1() {
+        System.out.println("master");
+        return "master";
+    }
+}
